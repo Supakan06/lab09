620c120e65a15bc7ea89f5354434148e2e5f17ea
Supakan06 <66160033@go.buu.ac.th>
2025-02-18 15:39:07 +0700

    feat: ดึงข้อมูลผู้ใช้จาก API ด้วย fetch

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const userList = document.getElementById("user-list");
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => {
+        // โค้ดสำหรับแสดงผู้ใช้
+    })
 });
