0cbf75ec3de00447fafde64eded06fa993a9eedf
Supakan06 <66160033@go.buu.ac.th>
2025-02-18 16:45:18 +0700

    feat: เพิ่ม event listener สำหรับการดูโพสต์ของผู้ใช้

--- user-detail.js
+++ user-detail.js
@@ -13,6 +13,9 @@ document.addEventListener("DOMContentLoaded", () => {
             <p><strong>เว็บไซต์</strong><br>${user.website}</p>
             <p><strong>ที่อยู่</strong><br>${user.address.street}, ${user.address.suite}<br>${user.address.city}, ${user.address.zipcode}</p>
             <p><strong>บริษัท</strong><br>${user.company.name}<br>${user.company.catchPhrase}</p>
-        `;
+            `;
+            document.getElementById("view-posts").addEventListener("click", () => {
+                window.location.href = `user-posts.html?id=${userId}`;
+            });
         })
 });
