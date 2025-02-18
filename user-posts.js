091ebcfed99bab12ce6a4acf17864bc6f4825b68
Supakan06 <66160033@go.buu.ac.th>
2025-02-18 17:10:32 +0700

    feat: ดึง query parameter และเลือก element สำหรับแสดงข้อมูลผู้ใช้

--- user-posts.js
+++ user-posts.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // โค้ดที่ต้องการให้ทำงานเมื่อหน้า HTML โหลดเสร็จแล้ว
+    const params = new URLSearchParams(window.location.search);
+    const userId = params.get("id");
+    const postsList = document.getElementById("posts-list");
+    const userNameElement = document.getElementById("user-name");
 })
